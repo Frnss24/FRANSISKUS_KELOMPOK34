a5f13f379142b1a069324f60503d98dc2f4fdc59
saph3n <vennandaazp@gmail.com>
2024-09-29 20:53:03 +0700

    Fix eksekusi

--- Simple ATM.py
+++ Simple ATM.py
@@ -55,5 +55,5 @@ def kelola_rekening_bank():
             break  
         else:
             print("Opsi tidak valid. Silakan coba lagi.")
-        if _name_ == "_main_":
+        if __name__ == "_main_":
             kelola_rekening_bank() 
